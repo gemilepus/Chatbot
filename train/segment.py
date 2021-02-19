86c3fab5341d57e087cf38ad36493c2b47bb7520
gemilepus <31269075+gemilepus@users.noreply.github.com>
2021-02-19 15:30:53 +0800

    add train code

---
+++ train/segment.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import jieba
+import logging
+
+def main():
+
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+    # jieba custom setting.
+    jieba.set_dictionary('jieba_dict/dict.txt.big')
+
+    # load stopwords set
+    stopwordset = set()
+    with open('jieba_dict/stopwords.txt','r',encoding='utf-8') as sw:
+        for line in sw:
+            stopwordset.add(line.strip('\n'))
+
+    output = open('wiki_seg.txt','w',encoding='utf-8')
+    
+    texts_num = 0
+    
+    with open('wiki_zh_tw.txt','r',encoding='utf-8') as content :
+        for line in content:
+            words = jieba.cut(line, cut_all=False)
+            for word in words:
+                if word not in stopwordset:
+                    output.write(word +' ')
+            texts_num += 1
+            if texts_num % 10000 == 0:
+                logging.info("已完成前 %d 行的斷詞" % texts_num)
+    output.close()
+    
+if __name__ == '__main__':
+	main()
